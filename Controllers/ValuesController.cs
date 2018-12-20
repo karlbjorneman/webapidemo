301f34186cf549fe6e678440b0aadf5689429a91
Karl Björneman <Karl.Bjorneman@milestone.dk>
2018-12-20 08:42:49 +0100

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// using System.Web.Http.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapidemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Apple", "Banana", "Orange" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        [AcceptVerbs("POST", "OPTIONS")]
+        public void Post([FromBody] Fruit value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
